7ddf998b08617d1c2a6768fc765603c5490717dc
ANUWAD PEERAPAN <anuwad.p@srisawadpower.co.th>
2024-04-17 08:59:58 +0700

    up

--- line-notify/index.js
+++ line-notify/index.js
@@ -1,66 +1,25 @@
-const http = require("http");
-const url = require("url");
-const client = require("prom-client");
+const express = require('express');
+const app = express();
+const axios = require('axios');
 
-const register = new client.Registry();
+app.post('/', (req, res) => {
+  axios.get('http://localhost:9093/api/v1/alerts')
+    .then(function (response) {
+      //กำหนดให้ส่งข้อมูลไปตามจำนวน array ที่ได้จาก pesponse
+      for (let i = 0; i < response.data.data.length; i++) {
+        //กำหนดข้อมูลที่จะส่งไป
+        const data = response.data.data[i].annotations.summary;
+        axios.get('http://areadiv.com/demo9/send.php?pram=' + data).then(response => {
+          console.log(data);
+        }).catch(error => {
+          console.log('Send data to line failed'+i);
+        });
 
-register.setDefaultLabels({
-  app: "example-nodejs-app2",
+      }
+    })
 });
 
-client.collectDefaultMetrics({ register });
 
-const httpRequestDurationMicroseconds = new client.Histogram({
-  name: "http_request_duration_seconds",
-  help: "Duration of HTTP requests in seconds",
-  labelNames: ["method", "route", "code"],
-  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10], // 0.1 to 10 seconds
-});
-
-register.registerMetric(httpRequestDurationMicroseconds);
-
-const createOrderHandler = async (req, res) => {
-  try {
-    if (req.url === '/user') {
-      console.log("User data retrieved successfully");
-      res.end("User data retrieved successfully");
-    } else if (req.url === '/post') {
-      console.log("post data retrieved successfully");
-      res.end("Post data retrieved successfully");
-    } else if (req.url === '/memo') {
-      console.log("memo data retrieved successfully");
-      res.end("Memo data retrieved successfully");
-    } else {
-      console.log("error data retrieved successfully");
-      res.writeHead(404).end("Not Found");
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    res.writeHead(500).end("Internal Server Error");
-  }
-};
-
-const server = http.createServer(async (req, res) => {
-  const end = httpRequestDurationMicroseconds.startTimer();
-  const route = url.parse(req.url).pathname;
-
-  try {
-    if (route === "/metrics") {
-      res.setHeader("Content-Type", register.contentType);
-      res.end(register.metrics());
-    } else {
-      await createOrderHandler(req, res);
-    }
-  } catch (error) {
-    res.writeHead(500).end();
-  }
-
-  // console.log('data = ' +'code = '+ route +'status = '+ res.statusCode+ ' method = '+ req.method);
-  end({ route, code: res.statusCode, method: req.method });
-});
-
-server.listen(8080, () => {
-  console.log(
-    "Grafana is running on http://localhost:3000, Server is running on http://localhost:8080, metrics are exposed on http://localhost:8080/metrics"
-  );
+app.listen(8082, () => {
+  console.log(`Server is listening at http://localhost:8082`);
 });
