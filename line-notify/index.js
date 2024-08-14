0fb34f640158e70c9fb20ab21cd8902299408a6c
anuwadYO276 <158416918+anuwadYO276@users.noreply.github.com>
2024-08-14 09:31:33 +0700

    update

--- line-notify/index.js
+++ line-notify/index.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const port = 1103;
 
 app.post('/', (req, res) => {
-  axios.get('http://localhost:9093/api/v1/alerts')
-    .then(function (response) {
-      //กำหนดให้ส่งข้อมูลไปตามจำนวน array ที่ได้จาก pesponse
-      for (let i = 0; i < response.data.data.length; i++) {
-        //กำหนดข้อมูลที่จะส่งไป
-        const data = response.data.data[i].annotations.summary;
-        axios.get('http://areadiv.com/demo9/send.php?pram=' + data).then(response => {
-          console.log(data);
-        }).catch(error => {
-          console.log('Send data to line failed'+i);
-        });
+  // axios.get('http://localhost:9093/api/v1/alerts')
+  //   .then(function (response) {
+  //     console.log(response.data);
+  //     res.send(response.data);
 
-      }
-    })
+  //   })
+
+  res.send('Hello World!');
+
 });
 
 
-app.listen(8082, () => {
-  console.log(`Server is listening at http://localhost:8082`);
+app.listen(port, () => {
+  console.log('Server is listening at http://localhost:' + port);
 });
